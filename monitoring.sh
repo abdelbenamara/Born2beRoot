43f374e7e038f092c064b4c6c35e2b615bb10701
abdelbenamara <abdelb.contact@gmail.com>
2023-01-02 12:22:30 +0000

    Updated monitoring

--- monitoring.sh
+++ monitoring.sh
@@ -10,7 +10,7 @@ main () {
 	cat /proc/loadavg | awk -v processors="$processors" '{ printf "#CPU load : %.1f%%\n", $1 / processors * 100 }'
 	echo "#Last boot : $(who -b | grep -o '....-..-.. ..:..')"
 	lsblk | grep 'lvm' | wc -l | awk '{ printf "#LVM use : %s\n", ($1 > 0 ? "yes" : "no") }'
-	echo "#Connections TCP : $(netstat -t | grep -c 'ESTABLISHED') ESTABLISHED"
+	echo "#Connections TCP : $(ss -t | grep -c '^ESTAB') ESTABLISHED"
 	echo "#User log : $(users | wc -w)"
 	echo "#Network : IP $(hostname -I | cut -d ' ' -f 1) ($(cat /sys/class/net/$(ip route list | grep default | cut -d ' ' -f 5)/address))"
 	echo "#Sudo : $(grep -c sudo /var/log/auth.log) commands"
