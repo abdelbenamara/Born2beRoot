2b73fa87ab0e9366145d222f9acfeabeddbfc35f
abdelbenamara <abdelb.contact@gmail.com>
2023-01-02 02:41:00 +0000

    Updated monitoring

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-message () {
-	echo "#Architecture: $(uname -a)"
+main () {
+	echo "#Architecture : $(uname -a)"
 	echo "#CPU physical : $(grep 'physical id' /proc/cpuinfo | sort | uniq | wc -l)"
-	echo "#vCPU : $(grep '^processor' /proc/cpuinfo | wc -l)"
-	free -m --si | grep 'Mem' | awk '{ printf("#Memory Usage: %d/%dMB (%.2f%%)\n", $7, $2, $7/$2*100) }'
+	processors=$(grep -c '^processor' /proc/cpuinfo)
+	echo "#vCPU : $processors"
+	free --si -m | grep 'Mem' | awk '{ printf "#Memory Usage : %d/%dMB (%.2f%%)\n", $3, $2, $3 / $2 * 100 }'
+	df --si -m | grep '/$' | awk '{ printf "#Disk Usage : %d/%sMB (%s)\n", $3, $4, $5 }'
+	cat /proc/loadavg | awk -v processors="$processors" '{ printf "#CPU load : %.1f%%\n", $1 / processors * 100 }'
+	echo "#Last boot : $(who -b | grep -o '....-..-.. ..:..')"
+	lsblk | grep 'lvm' | wc -l | awk '{ printf "#LVM use : %s\n", ($1 > 0 ? "yes" : "no") }'
+	echo "#Connections TCP : $(netstat -t | grep -c 'ESTABLISHED') ESTABLISHED"
+	echo "#User log : $(users | wc -w)"
+	echo "#Network : IP $(hostname -I | cut -d ' ' -f 1) ($(cat /sys/class/net/$(ip route list | grep default | cut -d ' ' -f 5)/address))"
+	echo "#Sudo : $(grep -c sudo /var/log/auth.log) commands"
 }
 
-message
+main | wall
+
+exit $?
