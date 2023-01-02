72771ec3b2e5ac9311d7147bbf5de8318370cba4
abdelbenamara <abdelb.contact@gmail.com>
2023-01-02 12:00:59 +0000

    Updated setup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 main () {
 	apt update
-	apt install python3
-	apt install python3-pip
+	apt install python3 -y
+	apt install python3-pip -y
 	pip install --upgrade pip setuptools wheel
-	pip install --user ansible-core
+	pip install ansible-core
 	wget https://raw.githubusercontent.com/abdelbenamara/Born2beRoot/main/born2beroot-debian-setup.yml
 	ansible-playbook born2beroot-debian-setup.yml
 }
